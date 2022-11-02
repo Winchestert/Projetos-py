81d842f31eabafd111a45d47a82de053e59b1e67
Daniel.rb <114115319+Winchestert@users.noreply.github.com>
2022-11-02 11:55:04 -0300

    Espaços

--- adivinha.py
+++ adivinha.py
@@ -1,21 +1,21 @@
 from random import randint
 from time import sleep
 
-#Welcome
-print('Seja bem vindo ao jogo da adivinhação! ')
+# Welcome
+print('Seja bem vindo ao jogo da adivinhação!')
 
-#Fazer o computador pensar
+# Fazer o computador pensar
 print('_-_' *15)
 pc = randint(0, 10)
 print('Tente adivinhar um numero de 0 a 10 !')
 print('_-_' *15)
 
-#Agora o jogador tenta adivinhar
+# Agora o jogador tenta adivinhar
 gamer = int(input('Qual número que eu pensei ? *-*: '))
 print('Um momento...')
 sleep(1)
 
-#As condições
+# As condições
 if gamer == pc:
     print('Parabéns!!! Você adivinhou.!.! \o/')
 else:
