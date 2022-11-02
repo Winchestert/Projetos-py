feb24dcef62096674cfcfbfa2ff95fe9c66cc3c2
Daniel.rb <daniel_sk8_nicolini@hotmail.com>
2022-11-02 12:03:49 -0300

    Remoção do ponto de exclamação

--- adivinha.py
+++ adivinha.py
@@ -2,12 +2,12 @@ from random import randint
 from time import sleep
 
 # Welcome
-print('Seja bem vindo ao jogo da adivinhação!')
+print('Seja bem vindo ao jogo da adivinhação')
 
 # Fazer o computador pensar
 print('_-_' *15)
 pc = randint(0, 10)
-print('Tente adivinhar um numero de 0 a 10 !')
+print('Tente adivinhar um numero de 0 a 10')
 print('_-_' *15)
 
 # Agora o jogador tenta adivinhar
