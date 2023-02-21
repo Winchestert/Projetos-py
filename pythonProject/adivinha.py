23aea5dcdd6ddd1528826231b31863d23fa99c8d
Daniel N. Ferraz <114115319+Winchestert@users.noreply.github.com>
2023-02-21 14:15:56 -0300

    Adicionando Arquivos
    Inserir arquivos no repositório.

---
+++ pythonProject/adivinha.py
@@ -0,0 +1,22 @@
+from random import randint
+from time import sleep
+
+# Welcome
+print('Seja bem vindo ao jogo da adivinhação')
+
+# Fazer o computador pensar
+print('_-_' *15)
+pc = randint(0, 10)
+print('Tente adivinhar um numero de 0 a 10')
+print('_-_' *15)
+
+# Agora o jogador tenta adivinhar
+gamer = int(input('Qual número que eu pensei ? *-*: '))
+print('Um momento...')
+sleep(1)
+
+# As condições
+if gamer == pc:
+    print('Parabéns!!! Você adivinhou.!.! \o/')
+else:
+    print('Oh, que pena, eu pensei no número {}, e não no número {}. '.format(pc, gamer))
